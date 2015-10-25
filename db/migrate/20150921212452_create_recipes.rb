7f0905914d2cea69edb4b1601562117363d57856
anntr <ania.trap@gmail.com>
2015-10-25 21:09:21 +0100

    zaraz sie poplacze

--- db/migrate/20150921212452_create_recipes.rb
+++ db/migrate/20150921212452_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.string :name
       t.text :instructions
-      t.integer :category
+      t.string :category, array: true, default: []
       t.integer :calories
     end
   end
